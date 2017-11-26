413aedaff3d30cbeb2db64fe0411d5ab4b7632f1
Whats-In-My-Vape <kevin.power.97.kp@gmail.com>
2017-11-26 21:11:52 +0000

    ++ Menu.Main|started

--- src/project/Main.java
+++ src/project/Main.java
@@ -5,8 +5,19 @@ package project;
         import javafx.fxml.FXMLLoader;
         import javafx.scene.Parent;
         import javafx.scene.Scene;
-        import javafx.scene.control.*;
+        import javafx.scene.control.Label;
         import javafx.scene.layout.BorderPane;
+        import javafx.scene.control.MenuItem;
+        import javafx.scene.control.Menu;
+        import javafx.scene.control.MenuBar;
+        import javafx.scene.control.SeparatorMenuItem;
+        import javafx.scene.control.TextField;
+        import javafx.geometry.Insets;
+        import javafx.geometry.Pos;
+        import javafx.scene.layout.Background;
+        import javafx.scene.layout.BackgroundFill;
+        import javafx.scene.layout.CornerRadii;
+        import javafx.scene.paint.Color;
         import javafx.stage.Stage;
 
 
@@ -20,10 +31,20 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         window = primaryStage;
         window.setTitle("Better IMDB");
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+       Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+
+
+
+//        bp.setTop(x=new Label("Air"));
+//        x.setAlignment(Pos.CENTER);
+//        x.setBackground(new Background(new BackgroundFill(Color.BISQUE,
+//                new CornerRadii(4),new Insets(3))));
+//        x.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+//        x.setPrefHeight(50);
+
 
         //Search Menu
-        Menu searchMenu = new Menu("search");
+        Menu searchMenu = new Menu("_search");
 
         //Menu Items
         MenuItem newFile = new MenuItem("New");
