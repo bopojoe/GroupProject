0b554b9f7ddc2660eee3f3d1dfbf65f2891aaf68
Whats-In-My-Vape <kevin.power.97.kp@gmail.com>
2017-11-26 17:51:42 +0000

    ++ Menu.Main|started

--- src/project/Main.java
+++ src/project/Main.java
@@ -5,22 +5,46 @@ package project;
         import javafx.fxml.FXMLLoader;
         import javafx.scene.Parent;
         import javafx.scene.Scene;
-        import javafx.scene.control.Button;
+        import javafx.scene.control.*;
+        import javafx.scene.layout.BorderPane;
         import javafx.stage.Stage;
 
 
 
 public class Main extends Application {
 
-    Button button;
+    Stage window;
+    BorderPane layout;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        button = new Button("Add");
+        window = primaryStage;
+        window.setTitle("Better IMDB");
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("IMDB");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+
+        //Search Menu
+        Menu searchMenu = new Menu("search");
+
+        //Menu Items
+        MenuItem newFile = new MenuItem("New");
+                newFile.setOnAction(e -> System.out.println("Create a new file"));
+        searchMenu.getItems().add(newFile);
+        searchMenu.getItems().add(new MenuItem("Actor"));
+        searchMenu.getItems().add(new SeparatorMenuItem());
+        searchMenu.getItems().add(new MenuItem("Movie"));
+        searchMenu.getItems().add(new SeparatorMenuItem());
+        searchMenu.getItems().add(new MenuItem("Exit"));
+
+        //Main menu bar
+        MenuBar menuBar = new MenuBar();
+        menuBar.getMenus().addAll(searchMenu);
+
+        layout = new BorderPane();
+        layout.setTop(menuBar);
+
+        Scene scene = new Scene(layout, 400,400);
+        window.setScene(scene);
+        window.show();
     }
 
 
