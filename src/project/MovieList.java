67be643d91a52039e22cc5f8dd42d2946909ddfa
bopojoe <devbopo.joe@gmail.com>
2017-11-28 00:20:02 +0000

    ++all working as was

--- src/project/MovieList.java
+++ src/project/MovieList.java
@@ -9,9 +9,9 @@ import java.util.Iterator;
 
 public class MovieList implements Iterable<Movie> {
 
-private int index;
 
-    protected  static LinkedList<Movie> hashlist[];
+
+    protected static LinkedList<Movie> hashlist[];
 
     static {
         hashlist = new LinkedList[50];
@@ -31,28 +31,28 @@ private int index;
     }
 
 
-   /* public void add()
+/*
+    public void add()
     {
-        LinkedList<Movie> innerList = hashlist.header.data;
+       // LinkedList<Movie> innerList = hashlist.header.data;
+}
+*/
+
+
+    public static void addMovie(String title, int year, int runningTime, String plot, String imgUrl){
+    Movie newMovie = new Movie(title,year, runningTime, plot,imgUrl);
+    int index = runningTime;
+    int hash =index%hashlist.length;
+    LinkedList<Movie> hashedList = hashlist[hash];
+    hashedList.add(newMovie);
+    System.out.println("You've just added:" + newMovie.toString()+"at Location: "+hash);
+
 }
 
 
-    public void addMovie(String title, int year, int runningTime, String plot, String imgUrl){
-    Movie newMovie = new Movie(title,year, runningTime, plot,imgUrl);
-    int index = runningTime;
-    movielist.addAtIndex(index,newMovie);
-    System.out.println("You've just added:" + newMovie.toString());
-    this.index ++;
-}*/
 
 
-/*protected void setup(int howMany){
 
-        for(int i =0;i!=howMany;i++){
-            hashlist.add(new LinkedList<>());
-        }
-
-    }*/
 
 
 }
